260c5fe6b16da812b062852ba1b079834a93b1f0
Harsh Deep <deep.harshdeep15@gmail.com>
2026-09-08 08:26:07 +0530

    Publish onboarding refinements from source 37d09b0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1443438578" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "180678644" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
